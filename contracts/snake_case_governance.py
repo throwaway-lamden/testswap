bfd284ffa9a5db98c37b9115c71db9b8d04881d4
throwaway-lamden <71742430+throwaway-lamden@users.noreply.github.com>
2023-07-28 08:32:38 -0400

    Fix typo ("governence" to "governance")

--- contracts/snake_case_governance.py
+++ contracts/snake_case_governance.py
@@ -1,16 +1,16 @@
     @export
-    def set_governence_token(address: str):
+    def set_governance_token(address: str):
         assert ctx.owner == ctx.caller
-        assert disable_governence_contract_change.get() == 0
-        governence_contract.set(address)
+        assert disable_governance_contract_change.get() == 0
+        governance_contract.set(address)
     
     @export
     def disable_contract_changes():
         assert ctx.owner == ctx.caller
-        disable_governence_contract_change.set(1)
+        disable_governance_contract_change.set(1)
     
     @export
     def forward_transaction_fee_rewards():
         assert disable_changing_contract.get() == 1
         amount = currency.balance_of(ctx.this)
-        currency.transfer(amount, governence_contract.get())
+        currency.transfer(amount, governance_contract.get())
